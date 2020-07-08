60e07bd6d51390b0eba13d697d8069c24fcabe3c
James Hopkins <jehopkin@gizmoj37.fhcrc.org>
2020-07-07 23:13:43 -0700

    added example files

--- run.sh
+++ run.sh
@@ -10,33 +10,17 @@ BASE_BUCKET="/fh/scratch/delete90/nelson_p/james/"
 ml nextflow
 
 NXF_VER=20.01.0 nextflow \
-    -c ../configs/nextflow.config \
     run \
     -resume \
     main.nf \
     -profile hpc \
     -work-dir $BASE_BUCKET/pdx/matched/work3/ \
-    --pdx true \
-    --matched true \
-    --cnvnator false \
-    --gatk_single_var false \
-    --input_csv ../manifests/lucap_manifest.txt \
-    --output_folder /fh/scratch/delete90/nelson_p/james/funcotating/cnvkit2/\
-    --pdx_reference $BASE_BUCKET/references/mm10/GRCm38.primary_assembly.genome.fa \
+    --input_csv ../manifests/lucap_manifest.csv \
+    --output_folder /fh/scratch/delete90/nelson_p/james/cnvkit/\
     --reference $BASE_BUCKET/references/hg38/Homo_sapiens_assembly38.fa \
     --reference_index $BASE_BUCKET/references/hg38/Homo_sapiens_assembly38.fa.fai \
     --reference_dict $BASE_BUCKET/references/hg38/Homo_sapiens_assembly38.dict \
-    --rear $BASE_BUCKET/references/hg38/1000G_phase1.snps.high_confidence.hg38.vcf.gz \
-    --rear_index $BASE_BUCKET/references/hg38/1000G_phase1.snps.high_confidence.hg38.vcf.gz.tbi \
-    --indels $BASE_BUCKET/references/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz \
-    --indels_index $BASE_BUCKET/references/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi \
-    --common_variants $BASE_BUCKET/references/hg38/af-only-gnomad.hg38.vcf.gz \
-    --common_variants_index $BASE_BUCKET/references/hg38/af-only-gnomad.hg38.vcf.gz.tbi \
-    --ref_name hg38 \
-    --contig_dict $BASE_BUCKET/references/gatk/Homo_sapiens_assembly38.dict \
     --input_beds local_capture.csv \
-    --clinvar /fh/scratch/delete90/nelson_p/james/references/hg38/clinvar_20200520_chr.vcf \
     --refFlat /fh/scratch/delete90/nelson_p/james/references/hg38/refFlat.txt \ 
-    --coords /home/jehopkin/nf-fh-pcp-wes/coords.txt \
 
 
